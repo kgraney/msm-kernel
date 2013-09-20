15361f30485e2b66cbe73a3a5e360b47b3fb5512
Kevin Graney <kmg2165@columbia.edu>
2013-09-20 19:04:05 -0400

    Fixed sys_fail() logic
    Corrected behavior for n==0.

--- kernel/sys_fail.c
+++ kernel/sys_fail.c
@@ -1,13 +1,19 @@
 #include <linux/syscalls.h>
 #include <linux/errno.h>
-//#include <linux/sched.h>
 
 SYSCALL_DEFINE1(fail, int, n)
 {
-	if (n < 0)
-		return -EINVAL;
+	if (n > 0) {
+		/* fail the nth system call */
+		current->num_to_fault = n;
+		current->flags |= PF_FAULT_CALL;
+		return 0;
 
-	current->num_to_fault = n;
-	current->flags |= PF_FAULT_CALL;
-	return 0;
+	} else if (n == 0) {
+		/* stop fault injection session */
+		current->flags &= ~PF_FAULT_CALL;
+		return 0;
+	}
+
+	return -EINVAL;
 }
