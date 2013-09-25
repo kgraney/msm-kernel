d28deae91b9e7dc19296834f764e603e49d92e69
Kevin Graney <kmg2165@columbia.edu>
2013-09-25 11:40:05 -0400

    Clear fault injection session on fork()
    Clear the fault injection session when the process is forked.  The
    Piazza question at the link below requests this feature:
    https://piazza.com/class/hhsygu5tiw858h?cid=127

--- kernel/fork.c
+++ kernel/fork.c
@@ -1297,6 +1297,9 @@ static struct task_struct *copy_process(unsigned long clone_flags,
 	p->memcg_batch.memcg = NULL;
 #endif
 
+	/* Clear any active fault injection session in the child. */
+	p->flags &= ~PF_FAULT_CALL;
+
 	/* Perform scheduler related setup. Assign this task to a CPU. */
 	sched_fork(p);
 
