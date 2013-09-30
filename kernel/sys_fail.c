2f6366b3881c6dc820751dab2459457e2703cb41
Kevin Graney <kmg2165@columbia.edu>
2013-09-29 20:19:39 -0400

    Added comments from w4118_hw2_jdmartin

--- kernel/sys_fail.c
+++ kernel/sys_fail.c
@@ -1,6 +1,13 @@
+/*
+ *  linux/arch/arm/kernel/sys_fail.c
+ */
 #include <linux/syscalls.h>
 #include <linux/errno.h>
 
+/*********************************************************************
+ * System call to inject system call faults N calls from the injection
+ * call (i.e. when sys_fail() is called).
+ ********************************************************************/
 SYSCALL_DEFINE1(fail, int, n)
 {
 	if (n > 0) {
@@ -18,11 +25,21 @@ SYSCALL_DEFINE1(fail, int, n)
 	return -EINVAL;
 }
 
+/*********************************************************************
+ * This system call routine returns 1 if current system call should be
+ * failed, and otherwise returns 0, after bookkeeping the number of
+ * calls to target attribute (i.e. counter).
+ ********************************************************************/
 long should_fail(void) {
 	return (current->flags & PF_FAULT_CALL) &&
 			--current->num_to_fault == 0;
 }
 
+/*********************************************************************
+ * This system call routine returns an artificial error code, indicat-
+ * ing a system call has failed, after terminating the fault injection
+ * session.
+ ********************************************************************/
 long fail_syscall(void) {
 	current->flags &= ~PF_FAULT_CALL;
 	return -EINVAL;
