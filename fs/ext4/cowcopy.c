b59fe1a434caa37b6aa923917438a9ffb103f5ef
Kevin Graney <kmg2165@columbia.edu>
2013-11-24 20:23:12 -0500

    Make use of existing linkat system call
    No need to reinvent things; this is much less code.

--- fs/ext4/cowcopy.c
+++ fs/ext4/cowcopy.c
@@ -4,6 +4,7 @@
 #include <linux/sched.h>
 #include <linux/fs_struct.h>
 #include <linux/path.h>
+#include <linux/fsnotify.h>
 
 static struct dentry* __get_dentry(const char *path_str, int flags, int *err)
 {
@@ -20,11 +21,9 @@ static struct dentry* __get_dentry(const char *path_str, int flags, int *err)
 
 SYSCALL_DEFINE2(ext4_cowcopy, const char __user, *src, const char __user, *dest)
 {
-	struct dentry *src_dent, *dest_dent;
-	struct inode *src_inode, *dest_dir;
-	struct path dest_path;
+	struct dentry *src_dent;
+	struct inode *src_inode;
 	int err;
-	char buf[512], *s;
 
 	if (!access_ok(VERIFY_READ, src, 1) || !access_ok(VERIFY_READ, dest, 1))
 		return -EINVAL;
@@ -41,41 +40,8 @@ SYSCALL_DEFINE2(ext4_cowcopy, const char __user, *src, const char __user, *dest)
 
 	/* TODO: check fs type of src_inode is ext4 */
 
-	dest_dent = kern_path_create(AT_FDCWD, dest, &dest_path, 0);
-	if (IS_ERR(dest_dent)) {
-		err = PTR_ERR(dest_dent);
-		goto err;
-	}
-
-	dest_dir = dest_dent->d_parent->d_inode;
-	if (!dest_dir) goto err;
-
-	s = dentry_path_raw(dest_dent, buf, 512);
-	printk(KERN_WARNING "COW: dest=%s -- %s", buf, s);
-	s = dentry_path_raw(src_dent, buf, 512);
-	printk(KERN_WARNING "COW: src=%s -- %s", buf, s);
-	s = dentry_path_raw(dest_dent->d_parent, buf, 512);
-	printk(KERN_WARNING "COW: dir=%s -- %s", buf, s);
-
-	printk(KERN_WARNING "COW: path:%p dest_dent:%p", dest_path.dentry, dest_dent);
-	s = dentry_path_raw(dest_path.dentry, buf, 512);
-	printk(KERN_WARNING "COW: path=%s -- %s", buf, s);
-
-	if (src_inode->i_sb != dest_dir->i_sb) {
-		err = -EXDEV;
-		goto err;
-	}
-
-	err = vfs_link(src_dent, dest_dir, dest_dent);
-	if (err) {
-		printk(KERN_WARNING "COW: error in vfs_link");
-		goto err;
-	}
-
-	printk(KERN_DEBUG "COW: src_inode=%p dest_inode=%p", src_dent->d_inode, dest_dent->d_inode);
-	path_put(&dest_path);
-	dput(dest_dent);
-	dput(src_dent);
+	err = sys_linkat(AT_FDCWD, src, AT_FDCWD, dest, 0);
+	if (err) goto err;
 
 	err = 1;
 	src_inode->i_op->setxattr(src_dent, "system.ext4_cow", &err, 1, 0);
